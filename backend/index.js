2482f1a7b4cb711ab142ff52454756388d34eab8
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2023-05-22 14:20:13 +0530

    added env variables

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(4000, (error) => {
+const port = process.env.PORT || 4000;
+app.listen(port, (error) => {
     if (error) {
         console.log(error.message);
     } else {
-        console.log(`Application is running on port 4000`);
+        console.log(`Application is running on port '${port}' and URL: '${process.env.BASE_URI}:${port}'`);
     }
 });
